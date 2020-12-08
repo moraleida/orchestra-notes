f27f3dd6ac33fd2db01c14abd172e616cebe4217
Ricardo Moraleida <ricardo.moraleida@10up.com>
2020-12-07 22:03:49 -0300

    elm format tests

--- tests/Example.elm
+++ tests/Example.elm
@@ -1,4 +1,4 @@
-module Example exposing ( guardianNames, suite )
+module Example exposing (guardianNames, suite)
 
 import Expect exposing (Expectation)
 import Fuzz exposing (Fuzzer, int, list, string)
@@ -8,11 +8,12 @@ import Test exposing (..)
 suite : Test
 suite =
     test "sum numbers" <|
-        \_ -> (2+2) |> Expect.equal 4
+        \_ -> (2 + 2) |> Expect.equal 4
+
 
 guardianNames =
     test "only 2 guardians have names w/ less than 6 chars" <|
-        \_ -> 
+        \_ ->
             let
                 guardians =
                     [ "Star-lord", "Groot", "Gamora", "Drax", "Rocket" ]
